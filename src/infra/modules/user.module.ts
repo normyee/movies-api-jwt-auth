682686156d68b53a9fa5b69b6fda1c7042326632
normyee <lunoqro@gmail.com>
2023-11-26 20:31:20 -0300

    chore: add nest/config, env added

--- src/infra/modules/user.module.ts
+++ src/infra/modules/user.module.ts
@@ -1,21 +1,23 @@
 import { Module } from '@nestjs/common';
 import { UserController } from '../http/controller/user.controller';
 import { UserServicee } from '../../application/services/user.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { UserEntity } from '../data/user.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'pgdb',
-      autoLoadEntities: true, // only for development purposes!!
+      type: process.env.TYPEORM_CONNECTION,
+      host: process.env.TYPEORM_HOST,
+      port: process.env.TYPEORM_PORT,
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      autoLoadEntities: true,
       synchronize: true,
-    }),
+    } as TypeOrmModuleOptions),
     TypeOrmModule.forFeature([UserEntity]),
   ],
   controllers: [UserController],
