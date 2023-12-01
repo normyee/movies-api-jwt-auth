1a69ba01d748ce132c4222e4ee585ca910aabd97
normyee <lunoqro@gmail.com>
2023-12-01 12:27:41 -0300

    chore: initialize caching in auth module

--- src/infra/modules/auth.module.ts
+++ src/infra/modules/auth.module.ts
@@ -7,11 +7,16 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from '../http/controller/auth.controller';
 import { LocalStrategy } from 'src/application/services/auth/strategies/local.strategy';
 import { JwtStrategy } from 'src/application/services/auth/strategies/jwt.strategy';
+import { MoviesModule } from './movies.module';
+import { redisStore } from 'cache-manager-redis-yet';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    CacheModule.register({ store: redisStore }),
     forwardRef(() => UserModule),
+    forwardRef(() => MoviesModule),
     PassportModule,
     JwtModule.register({
       privateKey: process.env.JWT_SECRET_KEY,
