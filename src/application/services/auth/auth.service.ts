004a2c9edfd2a4504e5f87de7e45e81f0ec43caf
normyee <lunoqro@gmail.com>
2023-12-02 19:38:33 -0300

    refactor: make code cleaner and easier to read

--- src/application/services/auth/auth.service.ts
+++ src/application/services/auth/auth.service.ts
@@ -1,13 +1,16 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { UserService } from '../user/user.service';
-import { compare } from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Cache } from 'cache-manager';
 import { UserEntity } from 'src/infra/data/user.entity';
+import { validatePasswords } from 'src/infra/helpers/password-comparer.helper';
 
 @Injectable()
 export class AuthService {
+  private readonly CACHE_TIME = 60000;
+  private readonly CACHE_PREFIX: 'cached:';
+
   constructor(
     private readonly userService: UserService,
     private readonly jwtService: JwtService,
@@ -15,7 +18,7 @@ export class AuthService {
   ) {}
 
   // tokenize login
-  async login(user) {
+  async login(user: UserEntity) {
     const payload = { sub: user.id, email: user.email };
 
     return {
@@ -26,37 +29,44 @@ export class AuthService {
   // local strategy method with passport for login validation
   async validateUser(email: string, password: string) {
     try {
-      const cachedUser = await this.cacheManager.get(`cached:${email}`);
+      const cachedUser = await this.cacheManager.get<UserEntity>(
+        `${this.CACHE_PREFIX}${email}`,
+      );
 
-      if (
-        cachedUser &&
-        typeof cachedUser === 'object' &&
-        'password' in cachedUser
-      ) {
-        const isPasswordValid = compare(
+      // checks if there is already a cached login
+
+      if (cachedUser) {
+        const isPasswordValid = await validatePasswords(
           password,
-          cachedUser.password as string,
+          cachedUser.password,
         );
 
         if (!isPasswordValid) {
           return null;
         }
 
-        return cachedUser as UserEntity;
+        return cachedUser;
       }
+
+      // If not found, it searches in the database
 
       const user = await this.userService.findByEmail(email);
 
-      const isPasswordValid = await compare(password, user.password);
+      const isPasswordValid = await validatePasswords(password, user.password);
 
       if (!isPasswordValid) {
         return null;
       }
 
-      await this.cacheManager.set(`cached:${email}`, user, 60000);
+      await this.cacheManager.set(
+        `${this.CACHE_PREFIX}${email}`,
+        user,
+        this.CACHE_TIME,
+      );
 
       return user;
     } catch (error) {
+      console.error(error);
       return null;
     }
   }
--- src/infra/http/controller/auth.controller.ts
+++ src/infra/http/controller/auth.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Post, UseGuards, Req } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from 'src/application/services/auth/auth.service';
+import { Request } from 'express';
+import { UserEntity } from 'src/infra/data/user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -8,7 +10,7 @@ export class AuthController {
 
   @UseGuards(AuthGuard('local'))
   @Post('login')
-  async login(@Req() req: any) {
+  async login(@Req() req: Request & { user: UserEntity }) {
     return await this.authService.login(req.user);
   }
 }
