0f8bccaecd40b4c3e225bbafb5f6184de42da5f3
normyee <lunoqro@gmail.com>
2023-12-06 07:26:31 -0300

    feat: add swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { UserModule } from './infra/modules/user.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(UserModule);
@@ -8,6 +9,18 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Movies Catalog API - Documentation')
+    .setDescription(
+      'This API serves as a movie catalog system integrated with JWT authentication. It provides a comprehensive collection of films and their details. Users can access and interact with the catalog after authentication through JSON Web Tokens (JWT). The API facilitates secure access to movie information, ensuring that only authorized users can utilize its functionalities.',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger-api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
